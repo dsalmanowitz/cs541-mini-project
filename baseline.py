f8f54e613059ee17534a82bf78038e7719cd4615
dsalmanowitz <salmanowitzd@gmail.com>
2022-04-19 22:53:23 -0400

    Update baseline.py
    Trimmed unnecessary code

--- baseline.py
+++ baseline.py
@@ -1,34 +1,24 @@
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.linear_model import LogisticRegression
-import numpy as np
-import nltk
 from nltk.corpus import stopwords
 import pandas as pd
-import random
-import re
-import string
+import time
+
+start_time = time.time()
 
 stopwords = set(stopwords.words('english'))
 
-mydata_train = fetch_20newsgroups(subset='train', shuffle=True)
-mydata_test = fetch_20newsgroups(subset='test', shuffle=True)
-mydata_train_df = pd.DataFrame({'data': mydata_train.data, 'target': mydata_train.target})
-mydata_test_df = pd.DataFrame({'data': mydata_test.data, 'target': mydata_test.target})
+training = fetch_20newsgroups(subset='train', shuffle=True)
+test = fetch_20newsgroups(subset='test', shuffle=True)
+training_df = pd.DataFrame({'data': training.data, 'target': training.target})
+test_df = pd.DataFrame({'data': test.data, 'target': test.target})
 
-tfidfV = TfidfVectorizer(stop_words=stopwords) 
-X_train_tfidfV = tfidfV.fit_transform(mydata_train_df.data) # fit_transform learns the vocab and one-hot encodes 
-X_test_tfidfV = tfidfV.transform(mydata_test_df.data) # transform uses the same vocab and one-hot encodes 
-
-X_train_tfidfV_df = pd.DataFrame(X_train_tfidfV.todense())
-X_train_tfidfV_df.columns = sorted(tfidfV.vocabulary_)
-
-# Performance of LR Classifier with No Stemming & Lemmatization
 text_clf = Pipeline([
         ('vect', TfidfVectorizer(stop_words=stopwords)),
         ('clf', LogisticRegression())])
 
-text_clf.fit(mydata_train_df.data, mydata_train_df.target)
-print(text_clf.score(mydata_test_df.data, mydata_test_df.target))
+text_clf.fit(training_df.data, training_df.target)
+print(text_clf.score(test_df.data, test_df.target))
+print(f'Time: {time.time()-start_time}')
